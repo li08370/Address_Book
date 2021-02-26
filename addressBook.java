2bbcdc94448420a0fa6a5320bfa270a957ea71cb
li08370 <43526854+li08370@users.noreply.github.com>
2021-02-26 08:26:37 -0800

    Add files via upload

--- addressBook.java
+++ addressBook.java
@@ -1,22 +1,31 @@
 //import java.util.;
-
+/* last adddrss printing 2 times*/
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+
 // Note will find objects that no longer exist
 public class addressBook {
     private static ArrayList<Address> addressList = new ArrayList<>();
+    private static final Address iAddress = new Address("n", "n", 0);
+    private Connection connect = null;
+    private Statement statement = null;
+    private ResultSet resultSet = null;
+    private PreparedStatement preparedStatement = null;
 
-
-    public static void main(String[] args) {
-        Address big = new Address("c", "three", 765);
-        Address the = new Address("b", "hell", 415);
-        Address he = new Address("a", "big", 654);
+    public static void main(String[] args) throws Exception {
+        addressBook ab = new addressBook();
+        ab.getData();
+        Address big = new Address("the", "three", 765);
+        Address the = new Address("the", "hell", 415);
+        Address he = new Address("binge", "big", 654);
 
         addressList.add(big);//index = 0
         addressList.add(the);
         addressList.add(he);
+
 
         int index;
         int action = 0;
@@ -35,6 +44,7 @@ public class addressBook {
                 action = input.nextInt();
             } catch (InputMismatchException exp) {
                 System.out.println("Please enter an action listed by the number");
+                input.next();
             }
             switch (action) {
                 case 1:
@@ -59,14 +69,16 @@ public class addressBook {
                     print();
                     break;
                 case 6:
-                   System.out.println("Loading");
+                    System.out.println("Loading");
                     break;
             }
             addressList.sort(Address::compareTo);
-        } while (action != 5) ;
-            for (Address a : addressList) {
-                System.out.println(a);
-            }
+        } while (action != 6);
+        for (Address a : addressList) {
+            System.out.println(a);
+        }
+        ab.readDB();
+        ab.close();
 
     }
 
@@ -121,57 +133,126 @@ public class addressBook {
         int pNumber;
         String name;
         int index = -1;
-        Address a = null;
+        String foundAt = null;
         ArrayList<Integer> found = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
-        if(!addressList.isEmpty()) {
+        if (!addressList.isEmpty()) {
             //User Input  used to search for an Address
             System.out.println("What address are you looking for? \n You can search by first name, last name or phone number");
             if (sc.hasNextInt()) {
                 pNumber = sc.nextInt();
-                a = new Address("n","n", pNumber);
+                iAddress.setPhone_number(pNumber);
 
             } else if (sc.hasNext()) {
                 name = sc.next();
-                a = new Address(name, name);
+                iAddress.setFirst_name(name);
+                iAddress.setLast_name(name);
             }
             //loop used to travers the address and find any address's that apply to the search
             for (int i = 0; i < addressList.size(); i++) {
-                assert a != null;
-
                 if (sName == false) {
-                    if (addressList.get(i).equals(a)) {
+                    if (addressList.get(i).equals(iAddress)) {
                         found.add(i);
                         System.out.println(addressList.get(i));
-                    }
-                } else {
-                    if (addressList.get(i).equal(a)){
-                        found.add(i);
-                        System.out.println(addressList.get(i));
+                    } else {
+                        if (addressList.get(i).equal(iAddress)) {
+                            found.add(i);
+                            System.out.println(addressList.get(i));
+                        }
                     }
                 }
             }
-        }
-        //determines whether there are any address that were added to the found array
-        if(found.size() == 0) {
-            System.out.println("There is not contact by that name or number");
-        }else if (type != 2 && (found.size() > 1)) {
+            //determines whether there are any address that were added to the found array
+            if (found.size() == 0) {
+                System.out.println("There is not contact by that name or number");
+            } else if (type != 2 && (found.size() > 1)) {
                 System.out.println("There are multiple contacts. Please use another attribute of the address?");
                 index = search(type, true);
             } else {
                 index = found.get(0);
             }
-        return index;
         }
+        return index;
+    }
 
-        public static void print(){
-            addressList.sort(Address::compareTo);
-            for (Address a : addressList) {
-                System.out.println(a);
+    public static void print() {
+        addressList.sort(Address::compareTo);
+        for (Address a : addressList) {
+            System.out.println(a);
+        }
+    }
+
+    public void getData() throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/addressDB", "root", "Teajay2023!");
+        statement = connect.createStatement();
+        resultSet = statement.executeQuery("select * from addressDB.addressT");
+        while (resultSet.next()) {
+            String first_name = resultSet.getString("first_name");
+            String last_name = resultSet.getString("last_name");
+            String phone_number = resultSet.getString("phone_number");
+            iAddress.setFirst_name(first_name);
+            iAddress.setLast_name(last_name);
+            iAddress.setPhone_number(Integer.parseInt(phone_number));
+            addressList.add(iAddress);
+        }
+        System.out.println(iAddress);
+        assert connect != null;
+        preparedStatement = connect.prepareStatement("truncate table addressT");
+        preparedStatement.executeUpdate();
+    }
+
+    private void writeResultSet(ResultSet resultSet) throws SQLException {
+        while (resultSet.next()) {
+            String first_name = resultSet.getString("first_name");
+            String last_name = resultSet.getString("last_name");
+            String phone_number = resultSet.getString("phone_number");
+
+            System.out.print("First name: " + first_name);
+            System.out.print("\t Last name: " + last_name);
+            System.out.print("\t Phone Number: " + phone_number);
+            System.out.print("\n\t --> ");
+        }
+    }
+
+    public void readDB()throws Exception{
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/addressDB", "root", "Teajay2023!");
+        statement = connect.createStatement();
+        resultSet = statement.executeQuery("select * from addressDB.addressT");
+        for(int i = 0; i < addressList.size(); i++){
+            preparedStatement = connect.prepareStatement("insert into addressDB.addressT values(default, ?, ?, ?)");
+            preparedStatement.setString(1, addressList.get(i).getFirst_name());
+            preparedStatement.setString(2, addressList.get(i).getLast_name());
+            preparedStatement.setString(3, String.valueOf(addressList.get(i).getPhone_number()));
+            preparedStatement.executeUpdate();
+        }
+       /* preparedStatement = connect.prepareStatement("DELETE FROM addressDB.addressT WHERE first_name = ? ;");
+        preparedStatement.setString(1, "");*/
+        preparedStatement = connect.prepareStatement("select * from addressDB.addressT");
+        resultSet = preparedStatement.executeQuery();
+        writeResultSet(resultSet);
+    }
+    private void close() {
+        try {
+            if (resultSet != null) {
+                resultSet.close();
             }
+            if (statement != null) {
+                statement.close();
+            }
+            if (connect != null) {
+                connect.close();
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
 
     }
+}
+
+
 
 
 
